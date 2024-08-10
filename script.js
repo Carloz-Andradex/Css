ad3c3538b52046ab1a67753ba60298cad0686942
Carloz-Andradex <andradecarlos2503@hotmail.com>
2024-08-09 23:26:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 onload = () => {
   document.body.classList.remove("container");
+
+  setTimeout(() => {
+    alert("Espero que te recuperes mi bonita, esto es para ti.💗");
+    window.location.href = "https://www.youtube.com/watch?v=Mcj75l2gJcY"; // Reemplaza con el enlace de tu elección
+  }, 30000); // 30000 milisegundos = 30 segundos
 };
